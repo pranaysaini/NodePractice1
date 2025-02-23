eb9765951736ffe5b68579462f5e7b787f52d4f7
Pranay Saini <sainipranay36@gmail.com>
2025-02-23 10:59:08 +0530

    practice project

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ app.post('/edit', function(req, res){
     });
 })
 
-
 app.post('/create', function(req, res){
    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
         res.redirect("/")
